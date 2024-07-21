2e602ae3f35fdf3a84146c2de35360e736629ecb
santosh punase <santoshpunase@gmail.com>
2024-07-21 22:49:46 +0530

    updated rate limit to 100

--- src/config/contants.js
+++ src/config/contants.js
@@ -2,7 +2,7 @@ export const SALT_ROUND = 10;
 
 export const RATE_LIMIT_CONFIG = {
   WINDOW_MS: 15 * 60 * 1000, // 15 minutes
-  MAX_REQUEST: 20, // limit each IP to 100 requests per windowMs
+  MAX_REQUEST: 100, // limit each IP to 100 requests per windowMs
   MAX_REQUEST_TEST: 1,
   MESSAGE: 'Too many requests from this IP, please try again later.',
 };
