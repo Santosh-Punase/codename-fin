d30e4c5f2dfa67408abb2801fe8ba628b2bfb70b
santosh punase <santoshpunase@gmail.com>
2024-07-23 22:21:40 +0530

    Added ip route

--- src/routes/healthRoute.js
+++ src/routes/healthRoute.js
@@ -3,5 +3,6 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (_req, res) => res.status(201).json({ message: 'Hello There!' }));
+router.get('/ip', (request, response) => response.send(request.ip));
 
 export default router;
