379c8363789147a936642aa809f60e080b5c812c
santosh punase <santoshpunase@gmail.com>
2024-08-21 14:02:04 +0530

    Reset and summary api fixes

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -5,13 +5,15 @@ import PaymentMode from '../models/PaymentMode.js';
 import logger from '../utils/logger.js';
 import { ERROR_CODES } from '../const/errorCodes.js';
 import { ERROR } from '../const/errorMessages.js';
+import { TRANSACTION_TYPE } from '../config/contants.js';
+import { defaultCategories, defaultPaymentModes } from '../utils/seed.js';
 
 export const getAccountSummary = async (req, res) => {
   try {
     const userId = req.user._id;
 
     const transactions = await Transaction.aggregate([
-      { $match: { userId } },
+      { $match: { user: userId } },
       {
         $group: {
           _id: '$type',
@@ -24,15 +26,15 @@ export const getAccountSummary = async (req, res) => {
     let totalExpense = 0;
 
     transactions.forEach((transaction) => {
-      if (transaction._id === 'income') {
+      if (transaction._id === TRANSACTION_TYPE.INCOME) {
         totalIncome = transaction.total;
-      } else if (transaction._id === 'expense') {
+      } else if (transaction._id === TRANSACTION_TYPE.EXPENSE) {
         totalExpense = transaction.total;
       }
     });
 
     const categories = await Category.aggregate([
-      { $match: { userId } },
+      { $match: { user: userId } },
       {
         $group: {
           _id: null,
@@ -73,13 +75,13 @@ export const getAccountSummary = async (req, res) => {
 };
 
 export const resetData = async (req, res) => {
+  const userId = req.user._id;
   try {
     const { resetTransactions, resetPaymentModes, resetCategories } = req.query;
-    const userId = req.user._id;
 
     if (!resetTransactions && !resetPaymentModes && !resetCategories) {
       logger.error(
-        `Reset account data error for user ${req.user.id}, No Reset Parameters provided`,
+        `Reset account data error for user ${userId}, No Reset Parameters provided`,
       );
 
       return res.status(500).json({
@@ -98,16 +100,25 @@ export const resetData = async (req, res) => {
     // Reset Payment Modes
     if (resetPaymentModes === 'true') {
       await PaymentMode.deleteMany({ user: userId });
+
+      // Insert default payment modes
+      const defaultModesWithUser = defaultPaymentModes.map((mode) => ({ ...mode, user: userId }));
+      await PaymentMode.insertMany(defaultModesWithUser);
     }
 
     // Reset Categories
     if (resetCategories === 'true') {
       await Category.deleteMany({ user: userId });
+
+      // Insert default categories
+      const defaultCategoriesWithUser = defaultCategories
+        .map((category) => ({ ...category, user: userId }));
+      await Category.insertMany(defaultCategoriesWithUser);
     }
 
     return res.status(200).json({ message: 'Reset completed successfully.' });
   } catch (err) {
-    logger.error(`Unable to reset account data for user ${req.user.id}, ${err}`);
+    logger.error(`Unable to reset account data for user ${userId}, ${err}`);
     return res.status(500).json({
       error: {
         code: ERROR_CODES.RESET_ACCOUNT_DATA_FAILED,
